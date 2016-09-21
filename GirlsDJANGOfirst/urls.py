b44f6e316abf80ab0b36b38116489513c0bde832
smallcrustation <lpmaze@gmail.com>
2016-09-21 00:32:03 -0400

    Django Vagins, fourth commit

--- GirlsDJANGOfirst/urls.py
+++ GirlsDJANGOfirst/urls.py
@@ -4,14 +4,15 @@ Definition of urls for GirlsDJANGOfirst.
 
 from django.conf.urls import include, url
 
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
     # Examples:
-    url(r'^$', GirlsDJANGOfirst.views.home, name='home'),
-    url(r'^GirlsDJANGOfirst/', include('GirlsDJANGOfirst.GirlsDJANGOfirst.urls')),
+    # url(r'^$', GirlsDJANGOfirst.views.home, name='home'),
+    #url(r'^GirlsDJANGOfirst/', include('GirlsDJANGOfirst.GirlsDJANGOfirst.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
